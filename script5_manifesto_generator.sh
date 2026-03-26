c04e573f926801e73be8c2c2956e185441deafca
yaswanth3322-art <yaswanth3322@gmail.com>
2026-03-27 00:36:50 +0530

    Update files

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script 5: Open Source Manifesto Generator
-# Author: Ajinkya Wasnik
+# Author: K Bala Yaswanth
 # Course: Open Source Software
 
 echo "Answer the following questions to generate your Open Source Manifesto."
